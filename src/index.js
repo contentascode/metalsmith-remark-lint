5653cca4f76d4ac1cc277afab9aecfa86b6de2a7
Jun Matsushita <jun@iilab.org>
2018-01-05 17:56:33 +0100

    Load .remarkrc and display pretty report

--- src/index.js
+++ src/index.js
@@ -1,21 +1,76 @@
-import {extname} from 'path';
+import { extname, join } from 'path';
+import engine from 'unified-engine';
+import vfile from 'vfile';
 import remark from 'remark';
-import html from 'remark-html';
+import report from 'vfile-reporter';
 
-export default function metalsmithRemark (plugins) {
-    return function (files, metal, done) {
-        Promise.all(Object.keys(files).map(file => {
-            const extension = extname(file);
-            if (extension !== '.md' && extension !== '.markdown') {
-                return true;
-            }
-            const markdown = String(files[file].contents);
-            return remark().use(plugins).use(html).process(markdown).then(result => {
-                files[file].contents = new Buffer(result.contents);
-                const data = files[file];
-                delete files[file];
-                files[file.replace(extension, '.html')] = data;
-            });
-        })).then(() => done());
+export default function metalsmithRemark(plugins) {
+    return function(files, metal, done) {
+        // const markdownFiles = Object.keys(files)
+        //     .filter(file => extname(file) === '.md' || extname(file) === '.markdown')
+        //     .map(file =>
+        //         vfile({ path: join(metal._source, file), contents: files[file].contents, cwd: metal._directory })
+        //     );
+        // console.log('markdownFiles', markdownFiles);
+        // engine(
+        //     {
+        //         processor: remark(),
+        //         cwd: metal._directory,
+        //         rcPath: join(metal._directory, '.remarkrc'),
+        //         files: markdownFiles,
+        //         // plugins,
+        //         color: true,
+        //         // quiet: true,
+        //         reporter: 'pretty',
+        //         reporterOptions: {
+        //             pretty: true
+        //         },
+        //         output: false
+        //     },
+        //     err => {
+        //         if (err) {
+        //             throw err;
+        //         }
+        //         done();
+        //     }
+        // );
+
+        Promise.all(
+            Object.keys(files).map(file => {
+                const extension = extname(file);
+                if (extension !== '.md' && extension !== '.markdown') {
+                    return true;
+                }
+                return engine(
+                    {
+                        processor: remark(),
+                        cwd: metal._directory,
+                        rcPath: join(metal._directory, '.remarkrc'),
+                        files: [
+                            vfile({
+                                path: join(metal._source, file),
+                                contents: files[file].contents,
+                                cwd: metal._directory
+                            })
+                        ],
+                        // plugins,
+                        color: true,
+                        quiet: true,
+                        reporter: 'pretty',
+                        reporterOptions: {
+                            pretty: true
+                        },
+                        output: false
+                    },
+                    err => {
+                        if (err) {
+                            done(err);
+                        }
+                    }
+                );
+            })
+        )
+            .then(() => done())
+            .catch(err => done(err));
     };
 }
